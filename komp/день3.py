9a4dddf08f351c3092ac31a1e62cca665d28e0d6
Your Name <you@example.com>
2025-07-27 22:45:38 +0300

    fufu

---
+++ komp/день3.py
@@ -0,0 +1,12 @@
+for i in range(5):
+    print(i)
+счетчик=5
+while счетчик >0:
+    print(счетчик)
+    счетчик-=1
+игры = ['Roblox']
+игры.append('Minecraft')
+игры.remove('Roblox')
+print(игры[0])
+for игра in игры:
+    print(игра)
